dea3063d61c6f084ca2b31bcd0910ac35c03b035
Web Firm Framework <webfirm.framework@gmail.com>
2018-11-18 13:21:06 +0530

    Production ready sample project with MUI css framework

---
+++ production-sample-with-mui-css-framework/src/main/java/com/wffwebdemo/minimalproductionsample/css/MUICssClass.java
@@ -0,0 +1,100 @@
+package com.wffwebdemo.minimalproductionsample.css;
+
+import com.webfirmframework.wffweb.tag.html.attribute.global.ClassAttribute;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import static com.webfirmframework.wffweb.util.StringUtil.splitBySpace;
+
+/**
+ * 
+ * Keeping an enum for css classes will prevent developer mistakes declaring
+ * class names. It will also help us to know what are the classes available in
+ * the whole project. <br>
+ * <br>
+ * It contains bootstrap4 classes but this is not complete. It may also contain
+ * bootstrap3 classes so use it carefully. NB: update it with bootstrap4 doc.
+ * You can also merge from custom classes, Eg:
+ * 
+ * <pre>
+ * <code>
+ * ClassAttribute classAttributeFromCssClasses = MUICssClass.BTN_DANGER.merge(CustomCssClass.CUSTOM_CLASS);
+ * </code>
+ * </pre>
+ *
+ */
+public enum MUICssClass implements CssClass {
+
+    BTN_PRIMARY("mui-btn mui-btn--primary"),
+
+    BTN_SMALL("mui-btn--small"),
+
+    BTN_RAISED("mui-btn mui-btn--raised"),
+
+    BTN_ACCENT("mui-btn mui-btn--accent"),
+
+    BTN_DANGER("mui-btn mui-btn--danger"),
+
+    FORM("mui-form"),
+
+    TEXTFIELD("mui-textfield"),
+
+    CONTAINER_FLUID("mui-container-fluid"),
+
+    ;
+
+    private final String[] classNames;
+
+    MUICssClass(String classNames) {
+        this.classNames = splitBySpace(classNames);
+    }
+
+    public String[] getClassNames() {
+        return classNames;
+    }
+
+    public ClassAttribute getClassAttribute() {
+        return new ClassAttribute(classNames);
+    }
+
+    public ClassAttribute getAttribute() {
+        return getClassAttribute();
+    }
+
+    public ClassAttribute merge(String... cssClasses) {
+
+        List<String> list = new ArrayList<>();
+        Collections.addAll(list, classNames);
+        list.addAll(Arrays.asList(cssClasses));
+
+        return new ClassAttribute(list.toArray(new String[list.size()]));
+    }
+
+    public ClassAttribute merge(CssClass... cssClasses) {
+
+        List<String> list = new ArrayList<>();
+        Collections.addAll(list, classNames);
+        for (CssClass cssClass : cssClasses) {
+            Collections.addAll(list, cssClass.getClassNames());
+        }
+
+        return new ClassAttribute(list.toArray(new String[list.size()]));
+    }
+
+    public ClassAttribute merge(CssClass[]... twoDArray) {
+
+        List<String> list = new ArrayList<>();
+        Collections.addAll(list, classNames);
+        for (CssClass[] cssClasses : twoDArray) {
+            for (CssClass cssClass : cssClasses) {
+                Collections.addAll(list, cssClass.getClassNames());
+            }
+        }
+
+        return new ClassAttribute(list.toArray(new String[list.size()]));
+    }
+
+}
---
+++ production-sample-with-mui-css-framework/src/main/java/com/wffwebdemo/minimalproductionsample/page/template/SampleTemplate2.java
@@ -0,0 +1,98 @@
+package com.wffwebdemo.minimalproductionsample.page.template;
+
+import com.webfirmframework.wffweb.tag.html.AbstractHtml;
+import com.webfirmframework.wffweb.tag.html.H3;
+import com.webfirmframework.wffweb.tag.html.attribute.Name;
+import com.webfirmframework.wffweb.tag.html.attribute.Type;
+import com.webfirmframework.wffweb.tag.html.attribute.event.ServerAsyncMethod;
+import com.webfirmframework.wffweb.tag.html.attribute.event.form.OnSubmit;
+import com.webfirmframework.wffweb.tag.html.formsandinputs.Button;
+import com.webfirmframework.wffweb.tag.html.formsandinputs.Form;
+import com.webfirmframework.wffweb.tag.html.formsandinputs.Input;
+import com.webfirmframework.wffweb.tag.html.formsandinputs.Label;
+import com.webfirmframework.wffweb.tag.html.html5.attribute.Placeholder;
+import com.webfirmframework.wffweb.tag.html.html5.attribute.Required;
+import com.webfirmframework.wffweb.tag.html.stylesandsemantics.Div;
+import com.webfirmframework.wffweb.tag.htmlwff.NoTag;
+import com.webfirmframework.wffweb.tag.repository.TagRepository;
+import com.webfirmframework.wffweb.wffbm.data.BMValueType;
+import com.webfirmframework.wffweb.wffbm.data.WffBMObject;
+import com.wffwebdemo.minimalproductionsample.css.MUICssClass;
+import com.wffwebdemo.minimalproductionsample.page.model.DocumentModel;
+
+@SuppressWarnings("serial")
+public class SampleTemplate2 extends Div implements ServerAsyncMethod {
+
+    private DocumentModel documentModel;
+
+    public SampleTemplate2(DocumentModel documentModel) {
+        super(null, MUICssClass.CONTAINER_FLUID.getAttribute());
+        this.documentModel = documentModel;
+        develop();
+    }
+
+ // @formatter:off
+    private void develop() {
+        changeTitle();
+        
+        new H3(this) {{
+            new NoTag(this, "SampleTemplate2.java");
+        }};
+        new Form(this,
+                MUICssClass.FORM.getAttribute(),
+            new OnSubmit("event.preventDefault(); return true;", 
+                this, 
+                "return {name:fullname.value};", 
+                "console.log(jsObject.msg); alert('The full name is printed in server console');")) {{
+            
+            new Div(this,
+                    MUICssClass.TEXTFIELD.getAttribute()) {{
+                    new Input(this,
+                        new Name("fullname"),
+                        new Type(Type.TEXT),
+                        new Required(),
+                        new Placeholder("Your Full Name"));
+                    new Label(this) {{
+                        new NoTag(this, "Full Name (Mandatory)");
+                    }};
+            }};
+            
+            new Button(this,
+                new Type(Type.SUBMIT),
+                MUICssClass.BTN_PRIMARY.getAttribute()) {{
+                new NoTag(this, "Submit");
+            }};
+        }};
+    }
+ // @formatter:on
+
+    private void changeTitle() {
+
+        // getTagRepository() will give object only if the browserPage.render is
+        // returned
+        TagRepository tagRepository = documentModel.getBrowserPage()
+                .getTagRepository();
+        if (tagRepository != null) {
+            AbstractHtml title = tagRepository.findTagById("windowTitleId");
+            if (title != null) {
+                title.addInnerHtml(new NoTag(null, "SampleTemplate2"));
+            }
+        }
+
+    }
+
+    @Override
+    public WffBMObject asyncMethod(WffBMObject data, Event event) {
+
+        System.out.println("full name: " + data.getValue("name"));
+
+        this.insertBefore(new SampleTemplate1(documentModel));
+        this.getParent().removeChild(this);
+
+        WffBMObject result = new WffBMObject();
+        result.put("msg", BMValueType.STRING,
+                "This msg will be printed in the browser console.");
+
+        return result;
+    }
+}
