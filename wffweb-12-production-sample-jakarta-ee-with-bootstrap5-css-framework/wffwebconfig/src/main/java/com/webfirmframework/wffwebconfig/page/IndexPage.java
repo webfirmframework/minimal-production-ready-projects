91095f6ebeae4308b6e5d38783db54cc0a10f1f4
Web Firm Framework <webfirm.framework@gmail.com>
2025-11-26 10:30:05 +0530

    Upgraded wffweb to 12.0.9.

--- wffweb-12-production-sample-jakarta-ee-with-bootstrap5-css-framework/wffwebconfig/src/main/java/com/webfirmframework/wffwebconfig/page/IndexPage.java
+++ wffweb-12-production-sample-jakarta-ee-with-bootstrap5-css-framework/wffwebconfig/src/main/java/com/webfirmframework/wffwebconfig/page/IndexPage.java
@@ -1,13 +1,13 @@
 package com.webfirmframework.wffwebconfig.page;
 
-import java.util.logging.Logger;
-
 import com.webfirmframework.ui.page.layout.IndexPageLayout;
 import com.webfirmframework.wffweb.server.page.BrowserPage;
 import com.webfirmframework.wffweb.server.page.BrowserPageSession;
 import com.webfirmframework.wffweb.tag.html.AbstractHtml;
 import com.webfirmframework.wffwebconfig.AppSettings;
 import com.webfirmframework.wffwebconfig.server.constants.ServerConstants;
+
+import java.util.logging.Logger;
 
 public class IndexPage extends BrowserPage {
 
@@ -16,8 +16,12 @@ public class IndexPage extends BrowserPage {
     @SuppressWarnings("unused")
     private static final Logger LOGGER = Logger.getLogger(IndexPage.class.getName());
 
-    // this is a standard interval
-    public static final int HEARTBEAT_TIME_MILLISECONDS = 25000;
+    // TODO this value is good for good internet speed, increase its value if you have poor internet.
+    public static final int HEARTBEAT_INTERVAL_MILLISECONDS = 10_000;
+
+    // TODO this value is good for good internet speed, increase its value if you have poor internet.
+    // Note: Make sure that your UI code doesn't block the UI update for more than 2000 milliseconds.
+    public static final int HEARTBEAT_TIMEOUT_MILLISECONDS = 2_000;
 
     private static final int WS_RECONNECT_TIME = 1000;
 
@@ -42,7 +46,8 @@ public class IndexPage extends BrowserPage {
         // Write BrowserPage configurations code here.
         // The following code can also be written inside render() method but
         // writing here will be a nice separation of concern.
-        super.setWebSocketHeartbeatInterval(HEARTBEAT_TIME_MILLISECONDS);
+        super.setWebSocketHeartbeatInterval(HEARTBEAT_INTERVAL_MILLISECONDS);
+        super.setWebSocketHeartbeatInterval(HEARTBEAT_TIMEOUT_MILLISECONDS);
         super.setWebSocketReconnectInterval(WS_RECONNECT_TIME);
         super.setExecutor(AppSettings.CACHED_THREAD_POOL);
     }
